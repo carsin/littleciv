356435e5716ffa8fe967252063124946555f7e49
Carson <carsonfreedman1@gmail.com>
2017-03-06 17:14:38 -1000

    Add click event handling

--- script/main.js
+++ script/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-	// init
 	mainMap = new Map(10, 10);
 	mainMap.init();
+
+	$("td").click(function() {
+		var xClicked = $(this).data("x");
+		var yClicked = $(this).data("y");
+
+		console.log("X: " + xClicked + " Y: " + yClicked);
+		
+		this.style.background = "green";
+	});
 });
