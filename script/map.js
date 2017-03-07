ed058fd483a94641930216c699fed57413996106
bananatrooper52 <bananatrooper52@gmail.com>
2017-03-06 16:17:19 -1000

    procedural map generation with js

---
+++ script/map.js
@@ -0,0 +1,17 @@
+var width = 12;
+var height = 12;
+
+function genMap() {
+  var str;
+  for(var y = 0; y < height; y++) {
+    str += "<tr>";
+    for(var x = 0; x < width; x++) {
+      str += "<td data-x='" + x + "' data-y='" + y + "'></td>";
+    }
+    str += "</tr>";
+  }
+  $("#map").html(str);
+
+}
+
+genMap();
