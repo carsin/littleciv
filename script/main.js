60533aa8af6f649d7d7bf60ce83b14c55387b149
Carson <carsonfreedman1@gmail.com>
2017-03-07 20:01:41 -1000

    Fix merge conflicts
    Elijah literally broke everything

--- script/main.js
+++ script/main.js
@@ -5,8 +5,7 @@ $(document).ready(function() {
 	var loc = window.location.href;
 	worldArgs = loc.substr(loc.indexOf("?") + 1, loc.length - 1).split("&");
 
-	mainMap = new Map(getValueOf("size"), getValueOf("size"));
-	mainMap.init();
+	mainMap = new Map(5, 5);
 	// INIT CELLS IN THEIR ID ORDER FOR ARRAY TO WORK PROPERLY
 	plains = new MapCell(0);
 
@@ -15,7 +14,6 @@ $(document).ready(function() {
 		var yClicked = $(this).data("y");
 
 		console.log("X: " + xClicked + " Y: " + yClicked);
-<<<<<<< HEAD
 		
 		//mainMap.changeCellId(xClicked, yClicked, 1);
 		tileClick(xClicked, yClicked);
@@ -31,13 +29,7 @@ $(document).ready(function() {
 		if (e.target !== this) return;
 
 		deselect(lastX, lastY);
-=======
-
-		this.style.background = "green";
-		mainMap.changeCellId(xClicked, yClicked, 1);
->>>>>>> origin/master
 	});
-	mainMap.changeCellId(0, 0, 1);
 });
 
 function getValueOf(value) {
